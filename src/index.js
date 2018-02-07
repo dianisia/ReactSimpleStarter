f5a5399aebd4a0098b08050eaa289d8d0e090b7d
dianisia <dianisia.m@yandex.ru>
2018-02-07 20:39:02 +0300

    Now show big video + better video list

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDRPAMykXbmpdM2Xzi8vRrEcNogv1ZTKzw';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfers'}, videos => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  }
 }
 
 ReactDom.render(<App />, document.querySelector('.container'));
